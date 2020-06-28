5648eb82a004bd31b12b4120bb13bc9d44fdaae4
Daniel Muyshond <daniel.muyshond@gmail.com>
2020-06-28 13:56:28 +0200

    [TELE-569] add compatibility check exception comment

--- passerelle_imio_aes_health/models.py
+++ passerelle_imio_aes_health/models.py
@@ -42,6 +42,7 @@ try:
     from xmlrpc.client import ServerProxy
 except ImportError:
     import xmlrpclib
+    # noinspection PyCompatibility
     from xmlrpclib import ServerProxy
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
